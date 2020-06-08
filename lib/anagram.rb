c735c884c853c0f9a5a04a65e9144c647f667090
seanbalke <sean.balke@gmail.com>
2020-06-08 17:21:42 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(matches)
+        matched_words = []
+        matches.each do |option|
+            if option.split("").sort == @word.split("").sort
+                matched_words << option
+            end
+        end
+        matched_words
+    end
+
+end
